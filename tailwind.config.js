825e7a4471c35bfd4c375b15ec9959769a53eb9f
Hgahlla <harmohangahlla@gmail.com>
2023-01-24 00:47:30 -0600

    Added spotify branding

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        spotify: {
+          green: "#1DB954",
+          black: "#191414",
+          white: "#FFFFFF",
+          gray: "#B3B3B3",
+          lightgray: "#282828",
+        },
+      },
+    },
   },
   plugins: [require("tailwind-scrollbar-hide"), require("@tailwindcss/forms")],
 };
